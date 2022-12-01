c097cb23d4aee2615c637d4aef0c2d139c44eb62
Shivronin <shivronin.stronz@gmail.com>
2022-12-01 20:24:49 +0700

    create

---
+++ app/src/main/java/com/example/pr23/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.pr23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    float x1, x2, y1, y2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        };
+
+
+    public boolean onTouchEvent(MotionEvent touchevent) {
+        switch (touchevent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchevent.getX();
+                y1 = touchevent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchevent.getX();
+                y2 = touchevent.getY();
+                if (x1 < x2) {
+                    Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(i);
+                }
+                if (x1 > x2) {
+                    Intent i = new Intent(MainActivity.this, MainActivity3.class);
+                    startActivity(i);
+                }
+                break;
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/pr23/MainActivity2.java
@@ -0,0 +1,46 @@
+package com.example.pr23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button buttonlist;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+    public void onClick(View view){
+
+        };
+
+    float x1, x2, y1, y2;
+    public boolean onTouchEvent(MotionEvent touchevent){
+        switch (touchevent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchevent.getX();
+                y1 = touchevent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchevent.getX();
+                y2 = touchevent.getY();
+                if (x1 < x2) {
+                    Intent i = new Intent(MainActivity2.this, MainActivity.class);
+                    startActivity(i);
+                }
+                break;
+        }
+        return  false;
+    }
+}
